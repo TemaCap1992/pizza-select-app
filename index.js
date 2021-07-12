6fa881ef6a0fdcd071a5b6b70aed4b088a17632b
Artem <TemaCap1992@github.com>
2021-07-12 18:37:46 +0300

    Init

---
+++ index.js
@@ -0,0 +1,88 @@
+import { Select } from './select/select'
+import './select/style.scss'
+
+export const selectedValue = {}
+export const selectedSize = {}
+export const selectedSous = {}
+
+const price = 10;
+let finishPrice = 0;
+
+const select = new Select('#select', {
+  placeholder: 'Выбери пожалуйста вид пиццы',
+  data: [
+    { id: '1', value: 'Пепперони', price: price },
+    { id: '2', value: 'Прошуто', price: price },
+    { id: '3', value: 'Куриная', price: price },
+    { id: '4', value: 'Деревенская', price: price }
+  ],
+  onSelect(item) {
+    selectedValue.pizza = {
+      value: item.value,
+      price: item.price
+    }
+    disabledSize.removeAttribute("disabled", "disabled")
+    finishPrice += item.price
+  }
+})
+
+const selectSize = new Select('#selectSize', {
+  placeholder: 'Выбери пожалуйста размер пиццы',
+  data: [
+    { id: '1', value: '16 cm ', price: 0 },
+    { id: '2', value: '24 cm ', price: 3 },
+    { id: '3', value: '30 cm ', price: 6 },
+    { id: '4', value: '35 cm ', price: 10 }
+  ],
+  onSelect(item) {
+    selectedSize.size = {
+      value: item.value,
+      price: item.price,
+    }
+    disabledSous.removeAttribute("disabled", "disabled")
+    finishPrice += item.price
+  }
+})
+
+const selectSous = new Select('#selectSous', {
+  placeholder: 'Выбери пожалуйста вид соуса',
+  data: [
+    { id: '1', value: 'Сырный', price: 1 },
+    { id: '2', value: 'Кисло-сладкий', price: 1 },
+    { id: '3', value: 'Барбекю', price: 1 },
+    { id: '4', value: 'Чесночный', price: 1 }
+  ],
+  onSelect(item) {
+    selectedSous.sous = {
+      value: item.value,
+      price: item.price
+    }
+    finishPrice += item.price
+  }
+})
+
+const btn = document.getElementById('btn')
+
+btn.onclick = () => {
+  const pizza = document.getElementById('pizza')
+  pizza.innerHTML = `Ваша пицца - " ${selectedValue.pizza.value} " (стоимость пиццы - ${selectedValue.pizza.price} BYN)`
+
+  const size = document.getElementById('size')
+  size.innerHTML = `Ваш размер - " ${selectedSize.size.value} " (доплата за размер + ${selectedSize.size.price} BYN)`
+
+  const sous = document.getElementById('sous')
+  sous.innerHTML = `Ваш соус - " ${selectedSous.sous.value} " (доплата за соус + ${selectedSous.sous.price} BYN)`
+
+  const sum = document.getElementById('sum')
+  sum.innerHTML = `Сумма Вашего заказа - " ${finishPrice} BYN "`
+  finishPrice = 0
+}
+
+const disabledSous = document.getElementById('selectSous')
+const disabledSize = document.getElementById('selectSize')
+
+const selectedPlaceholder = document.getElementById('placeholder')
+if (selectedPlaceholder.innerHTML === 'Выбери пожалуйста вид пиццы') {
+  disabledSize.setAttribute("disabled", "disabled")
+  disabledSous.setAttribute("disabled", "disabled")
+}
---
+++ select/select.js
@@ -0,0 +1,102 @@
+import { selectedValue } from "../index"
+import { selectedSize } from "../index"
+import { selectedSous } from "../index"
+
+const getTemplate = (data = [], placeholder) => {
+
+    const items = data.map(item => {
+        return `
+      <li class="select__item" data-type="item" data-id=${item.id}>${item.value} </li>
+    `
+    })
+
+    const text = placeholder ?? 'Сделайте выбор'
+    return ` 
+  <div class="select__input" data-type="input">
+    <span data-type="value" id="placeholder">${text}</span>
+    <i class="fa fa-chevron-down" data-type="arrow"></i>
+  </div>
+  <div class="select__dropdown">
+    <ul class="select__list">
+      ${items.join('')}
+    </ul>
+  </div>
+  `
+}
+
+export class Select {
+    constructor(selector, options) {
+        this.$el = document.querySelector(selector)
+        this.options = options
+        this.selectedId = null
+
+        this.#render()
+        this.#setup()
+    }
+
+    #render() {
+        const { placeholder, data } = this.options
+        this.$el.classList.add('select')
+        this.$el.innerHTML = getTemplate(data, placeholder)
+    }
+
+    #setup() {
+        this.clickHandler = this.clickHandler.bind(this)
+        this.$el.addEventListener('click', this.clickHandler)
+        this.$arrow = this.$el.querySelector('[data-type="arrow"]')
+        this.$value = this.$el.querySelector('[data-type="value"]')
+    }
+
+    clickHandler(event) {
+        const { type } = event.target.dataset
+        if (type === 'input' || type === 'value' || type === 'arrow') {
+            this.toggle()
+
+        } else if (type === 'item') {
+            const id = event.target.dataset.id
+            this.select(id)
+
+        } else if (type === 'backdrop') {
+            this.close()
+        }
+    }
+
+    get isOpen() {
+        return this.$el.classList.contains('open')
+    }
+
+    get current() {
+        return this.options.data.find(item => item.id === this.selectedId)
+    }
+
+    select(id) {
+        this.selectedId = id
+        this.$value.textContent = this.current.value
+        this.$el.querySelectorAll('[data-type="item"]').forEach(el => {
+            el.classList.remove('selected')
+        })
+        this.$el.querySelector(`[data-id="${id}"]`).classList.add('selected')
+        this.options.onSelect ? this.options.onSelect(this.current) : null
+        this.close()
+    }
+
+    toggle() {
+        this.isOpen ? this.close() : this.open()
+    }
+
+    open() {
+        this.$el.classList.add('open')
+        this.$arrow.classList.remove('fa-chevron-down')
+        this.$arrow.classList.add('fa-chevron-up')
+    }
+
+    close() {
+        this.$el.classList.remove('open')
+        this.$arrow.classList.remove('fa-chevron-up')
+        this.$arrow.classList.add('fa-chevron-down')
+    }
+
+    destroy() {
+        this.$el.removeEventListener('click', this.clickHandler)
+    }
+}
